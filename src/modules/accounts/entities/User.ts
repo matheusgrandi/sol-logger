086ea66194f2d4a35d6e476e01ea89bdb67e3f5f
matheusgrandi <matheus@matheusgrandi.com.br>
2022-05-16 17:12:24 -0300

    :sparkles: user entity

--- src/modules/accounts/entities/User.ts
+++ src/modules/accounts/entities/User.ts
@@ -1,1 +1,35 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  company: string;
+
+  @Column()
+  huxxToken?: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = v4();
+    }
+  }
+}

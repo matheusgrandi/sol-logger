6e101ad7d45538f2f2b4232f11df424e824fa943
matheusgrandi <matheus@matheusgrandi.com.br>
2022-05-23 09:24:47 -0300

    :recycle: routes refactor

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
-import { app } from '.';
+import express from 'express';
+import 'reflect-metadata';
+
+import { router } from 'routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+export { app };
 
 app.listen(3000, () => console.log('Server is running! 🚀'));

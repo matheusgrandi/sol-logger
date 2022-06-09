516ed4f2ef441846688e166c50926c90335d649d
matheusgrandi <matheus@matheusgrandi.com.br>
2022-06-09 17:54:42 -0300

    :sparkles: users migrations inserction

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,6 +2,9 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User } from '../modules/accounts/entities/User';
 
+//docker-compose exec app node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d src/database
+//yarn typeorm migration:create src/database/migrations/MigrationName
+
 const dataSource = new DataSource({
   type: 'postgres',
   host: 'database',

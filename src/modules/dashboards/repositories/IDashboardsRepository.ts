f123b208a7866be5cbb4967cf40b0bf9cd549e31
matheusgrandi <matheus@matheusgrandi.com.br>
2022-06-15 14:22:37 -0300

    :fire: dashboard function removal

--- src/modules/dashboards/repositories/IDashboardsRepository.ts
+++
@@ -1,7 +0,0 @@
-interface IDashboardsRepository {
-  getData(uid: string): Promise<object>;
-  setData(equipment: string): Promise<object>;
-  setAlert(equipment: string): Promise<object>;
-}
-
-export { IDashboardsRepository };
--- src/modules/dashboards/useCases/generateRelatory/GenerateRelatoryUseCases.ts
+++
@@ -1,9 +0,0 @@
-import { Dashboard } from "routes/modules/model/Dashboard"; 
-
-class GenerateRelatoryUseCase {
-  execute ({ url, os, equipment, init, end }: Dashboard) {
-    data = this.
-  }
-}
-
-export { GenerateRelatoryUseCase }
